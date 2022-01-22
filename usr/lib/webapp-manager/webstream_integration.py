a40457d605d23f574deef4d16d65fe94e255e5ec
pizzalovingnerd <pizzalovingnerd@teknik.io>
2022-01-21 23:12:08 -0800

    Finished web store - New icon - Added search function - Icons now transfer over from webstream (although they are downscaled to 64x64)

--- usr/lib/webapp-manager/webstream_integration.py
+++ usr/lib/webapp-manager/webstream_integration.py
@@ -1,7 +1,7 @@
 import webstream
 import gi
 import requests
-import textwrap
+import tempfile
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Pango, Gio, GdkPixbuf, Gdk
@@ -9,31 +9,42 @@ from gi.repository import Gtk, Pango, Gio, GdkPixbuf, Gdk
 webstream_url = "https://raw.githubusercontent.com/risiOS/risi-webstream-repo/main/repo.yml"
 icons_url = "https://raw.githubusercontent.com/risiOS/risi-webstream-repo/main/icons/{}.png"
 
+# A dictionary for the category tabs.
+tab_category = {
+    1: "Audio",
+    2: "Utility",
+    3: "Development",
+    4: "Education",
+    5: "Game",
+    6: "Graphics",
+    7: "Network",
+    8: "Office",
+    9: "Video"
+}
 
 class ListboxApp(Gtk.Box):
-    def __init__(self, app, main_window, store_window):
+    def __init__(self, app, main_window):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL)
         self.app = app
         self.main_window = main_window
-        self.store_window = store_window
 
         image = Gtk.Image.new_from_icon_name(
             "applications-internet",
             Gtk.IconSize.DIALOG
         )
 
-        pixbuf = pixbuf_from_url(
+        self.pixbuf = pixbuf_from_url(
             icons_url.format(app.appid)
         )
 
-        if not pixbuf:
+        if not self.pixbuf:
             image.set_from_pixbuf(
-                Gtk.IconTheme().load_icon("applications-internet", 64, Gtk.IconLookupFlags.FORCE_SIZE)
+                Gtk.IconTheme().load_icon("default-webapp", 64, Gtk.IconLookupFlags.FORCE_SIZE)
             )
         else:
-            pixbuf = pixbuf.scale_simple(64, 64, GdkPixbuf.InterpType.BILINEAR)
+            self.pixbuf = self.pixbuf.scale_simple(64, 64, GdkPixbuf.InterpType.BILINEAR)
             image.set_from_pixbuf(
-                pixbuf
+                self.pixbuf
             )
 
         nameAndTagBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
@@ -77,24 +88,32 @@ class ListboxApp(Gtk.Box):
         self.set_halign(Gtk.Align.FILL)
 
     def install_button(self, button):
+        # Generate icon
+        icon_path = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+        self.pixbuf.savev(
+            icon_path.name,
+            "png"
+        )
+
         self.main_window.on_add_button(button)
         self.main_window.name_entry.set_text(self.app.name)
         self.main_window.url_entry.set_text(self.app.url)
         self.main_window.category_combo.set_active(
             list(
-                self.store_window.tab_category.values()
+                tab_category.values()
             ).index(self.app.main_category)
         )
-        self.store_window.window.destroy()
+        self.main_window.icon_chooser.set_icon(icon_path.name)
+        self.get_toplevel().destroy()
 
     def url_button(self, button):
         Gtk.show_uri_on_window(self.store_window.window, self.app.homepage, Gdk.CURRENT_TIME)
+
 
 class StoreWindow:
     def __init__(self, main_window):
         self.main_window = main_window
         self.previous_tab = 0
-        # Gtk.ApplicationWindow.__init__(self)
 
         # Glade file
         self.gui = Gtk.Builder()
@@ -110,26 +129,17 @@ class StoreWindow:
         self.app_store = webstream.Storage()
         self.app_store.load_from_url(webstream_url)
 
-        # A dictionary for the category tabs.
-        self.tab_category = {
-            1: "Audio",
-            2: "Utility",
-            3: "Development",
-            4: "Education",
-            5: "Game",
-            6: "Graphics",
-            7: "Network",
-            8: "Office",
-            9: "Video"
-        }
-
         self.gui.get_object("tabs").connect("switch-page", self.tab_switched)
         self.load_featured(self.gui.get_object("featured_list"))
+
+        search_page = Search(self.app_store, self.main_window)
+        self.gui.get_object("searchpage").add(search_page)
+        self.gui.get_object("searchbox").connect("changed", lambda query: search_page.search(query.get_text()))
 
     def load_featured(self, page):
         for app in self.app_store.get_apps_by_tag("Featured"):
             app.main_category = app.categories[0]
-            page.add(ListboxApp(app, self.main_window, self))
+            page.add(ListboxApp(app, self.main_window))
 
     def tab_switched(self, notebook, page, page_id):
         for child in notebook.get_nth_page(self.previous_tab):
@@ -140,7 +150,7 @@ class StoreWindow:
         else:
             for app in self.app_store.get_apps_by_category(self.tab_category[page_id]):
                 app.main_category = self.tab_category[page_id]
-                page.add(ListboxApp(app, self.main_window, self))
+                page.add(ListboxApp(app, self.main_window))
         page.show_all()
 
         self.previous_tab = page_id
@@ -155,3 +165,34 @@ def pixbuf_from_url(url):
     return GdkPixbuf.Pixbuf.new_from_stream(
         Gio.MemoryInputStream.new_from_data(image.content, None)
     )
+
+
+class Search(Gtk.ListBox):
+    def __init__(self, app_store, main_window):
+        Gtk.ListBox.__init__(self)
+        self.app_store = app_store
+        self.main_window = main_window
+        self.set_valign(Gtk.Align.FILL)
+        self.set_vexpand(True)
+
+    def search(self, query):
+        self.reset()
+        if len(query) > 2:
+            for app in self.app_store.get_apps_by_search(query):
+                app.main_category = app.categories[0]
+                self.add(ListboxApp(app, self.main_window))
+        else:
+            search_label = Gtk.Label()
+            search_label.set_margin_top(10)
+            search_label.set_markup("<b>Please enter search query</b>")
+            search_label.set_margin_bottom(10)
+            self.add(search_label)
+
+        self.show_all()
+
+    def reset(self):
+        for child in self.get_children():
+            child.destroy()
+
+
+    # def add(self):
